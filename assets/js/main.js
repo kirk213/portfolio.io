a9d135975be4a55c565ca703bd9870af8f397ba4
kirk213 <karekun21@gmail.com>
2022-08-31 16:33:37 +0900

    8/31 update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,19 @@
 let menu = document.querySelector('.menu-icon');
+let bar = document.querySelectorAll('.menu');
 
-console.log(menu);
+//メニューアイコンにマウス載せた時に色変わる
+menu.addEventListener('mouseenter',function(){
+    bar.forEach(element => {
+        element.classList.toggle('cc');
+    });
+})
+menu.addEventListener('mouseleave',function(){
+    bar.forEach(element => {
+        element.classList.toggle('cc');
+    });
+})
 
+//メニューアイコンクリックで開く
 menu.onclick = function(){
     menu.classList.toggle('move');
 }
